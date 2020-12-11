e958c399fda3343adc5952865417fd851ebcc8f3
luis <bisbicusluis@gmail.com>
2020-12-11 00:34:28 -0500

    cuarto commit

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -18,7 +18,7 @@ created: function(){
     this.username = this.$route.params.username
     let self = this
 
-    axios.get("http://cajero-api.herokuapp.com/user/balance/" + this.username)
+    axios.get("http://cajero-api-lebg.herokuapp.com/user/balance/" + this.username)
             .then((result) => {
             self.balance = result.data.balance
         })
