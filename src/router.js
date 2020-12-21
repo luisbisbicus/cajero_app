92694bf55f29b924d963fc14bec6c1aa11197eb7
luis <bisbicusluis@gmail.com>
2020-12-20 22:08:52 -0500

    quinto commit

--- src/router.js
+++ src/router.js
@@ -2,10 +2,14 @@ import vueRouter from 'vue-router'
 import User from './components/User'
 import UserBalance from './components/UserBalance'
 import App from './App'
+import UserAuth from './components/UserAuth'
+import UserTransaction from './components/UserTransaction'
+
 const router = new vueRouter({
     mode: 'history',
     base: __dirname,
     routes: [
+        
         {
             path: '/',
             name: "root",
@@ -21,6 +25,17 @@ const router = new vueRouter({
             name: "user_balance",
             component: UserBalance
         },
+        {
+            path: '/user/auth',
+            name: "user_auth",
+            component: UserAuth
+        },
+        {
+            path: '/user/transaction/:username',
+            name: "user_transaction",
+            component: UserTransaction
+        },
+        
             ]
             })
         export default router
